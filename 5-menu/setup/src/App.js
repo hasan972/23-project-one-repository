f8285066a653c85c23561aa611a6a169d723c31b
Shanto <rakibulhasansanto25@gmail.com>
2021-05-30 23:46:53 +0600

    finish the project menu

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -2,21 +2,33 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
-  const [menuItem, setMenuItem] = useState(items)
-  const [categories, setCaptegories] = useState([])
-  return <main>
-    <section className='menu section'>
-      <div className='title'>
-        <h2>Oure Menu</h2>
-        <div className='underline'></div>
-      </div>
-      <Categories />
-      {/* //all the data is props for this */}
-      <Menu items={menuItem} />
-    </section>
-  </main>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
