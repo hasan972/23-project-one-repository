d5faad56f9f64368cf34c8133519b2db2bc973c2
Shanto <rakibulhasansanto25@gmail.com>
2021-06-02 23:33:07 +0600

    Data Add

--- 7-slider/setup/src/data.js
+++ 7-slider/setup/src/data.js
@@ -35,6 +35,16 @@ const people = [
     quote:
       'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
   },
+  {
+    id: 5,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+
 ];
 
 export default people;
