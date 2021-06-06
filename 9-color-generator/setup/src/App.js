4632cf5a85a08913872715684aa8948c969d2288
Shanto <rakibulhasansanto25@gmail.com>
2021-06-07 00:53:35 +0600

    onChange onSubmit event using

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -7,7 +7,26 @@ function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
   const [list, setList] = useState(new Values('#f15025').all(10))
-  return <h2>color generator project</h2>
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    let colors = new Values(color).all(10);
+    console.log(colors)
+  }
+  return (
+    <>
+      <section className="container">
+        <h3>Simple color genarator</h3>
+        <form onSubmit={handelSubmit}>
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#fff" />
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List of hare</h4>
+      </section>
+    </>
+  )
 }
 
 export default App
