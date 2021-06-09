5b78cb5c1153e4796ed95843e086e79d5f3e86e0
Shanto <rakibulhasansanto25@gmail.com>
2021-06-10 00:50:08 +0600

    Updated app file

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -9,7 +9,35 @@ function App() {
   const [editId, setEditId] = useState(null);
   //pas of an object for more option and more way to change the value and color
   const [alert, setAlert] = useState({ show: false, msg: " ", type: " " })
-  return <h2>grocery bud setup</h2>
+  const handelSubmit = (e) => {
+    e.preventDefault()
+    //show the display alert
+    if (!name) {
+      //for the name and edite
+    } else if (name && isEditing) {
+
+    } else {
+      //show the alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+    }
+  }
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handelSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Item</h3>
+        <div className='form-control'>
+          <input className='grocery' type='text' placeholder='eg:egg' value={name} onChange={(e) => setName(e.target.value)} />
+          <button type='submit' className='submit-btn' >{isEditing ? 'edit' : 'submit'}</button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear all items</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
