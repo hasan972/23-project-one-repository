6296fe4b53457c5e290ebce88f2400016a632f86
Shanto <rakibulhasansanto25@gmail.com>
2021-06-09 23:58:29 +0600

    add the 4 hook in app.js

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,6 +3,12 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  //pas of an object for more option and more way to change the value and color
+  const [alert, setAlert] = useState({ show: false, msg: " ", type: " " })
   return <h2>grocery bud setup</h2>
 }
 
